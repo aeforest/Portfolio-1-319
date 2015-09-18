1be4faa898143d6a8bbc3bd59fef57ea62400614
unknown <aeforest@CO1313-22.ece.iastate.edu>
2015-09-18 12:43:33 -0500

    more setup

---
+++ Portfolio1/src/jSnake/Model.java
@@ -0,0 +1,5 @@
+package jSnake;
+
+public class Model {
+
+}
---
+++ Portfolio1/src/jSnake/View.java
@@ -0,0 +1,51 @@
+package jSnake;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+
+public class View extends JFrame {
+
+	private JPanel contentPane;
+	private final JTable table = new JTable();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					View frame = new View();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public View() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblScore = new JLabel("Score: 0");
+		lblScore.setBounds(0, 0, 46, 14);
+		contentPane.add(lblScore);
+		table.setRowSelectionAllowed(false);
+		table.setBounds(0, 15, 434, 247);
+		contentPane.add(table);
+	}
+}
